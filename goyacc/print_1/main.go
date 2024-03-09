190e2760a8a83ace18011a9453386f1faebb956a
yongj.zhuang <yongj.zhuang@outlook.com>
2024-03-09 14:06:26 +0800

    Input from stdin

--- goyacc/print_1/main.go
+++ goyacc/print_1/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"demo/parser"
+	"os"
 )
 
 func main() {
-	s := `
-		s = 'ok' // comments: s = '???'
-		s = 'not ok'
-		print  'reading s'
-		print s
-	`
-	parser.Parse(s)
+	// s := `
+	// 	s = 'ok' // comments: s = '???'
+	// 	s = 'not ok'
+	// 	print  'reading s'
+	// 	print s
+	// `
+
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			inp := scanner.Text()
+			if inp == "exit" {
+				println("byte")
+				return
+			}
+			parser.Parse(inp)
+		}
+	}
 }
